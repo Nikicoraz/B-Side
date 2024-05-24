fd26fdf5a55dd374340b05a77e2645ee5069d34c
FratelloDog <123020177+FratelloDog@users.noreply.github.com>
2024-05-24 15:55:53 +0200

    user.php alpha

--- user.php
+++ user.php
@@ -1,8 +1,15 @@
 <?php 
+    include "php_scripts/grab_token.php";
+    include "php_scripts/search_album.php";
+    
+    $token = get_token();
+    session_start();
+
     if(isset($_GET['username']))
         $username = $_GET['username'];
     else
         die("<h1>User not found! :/</h1>");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,14 +17,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $username ?></title>
-    <link rel="stylesheet" href="css/comune.css">
+    <link rel="stylesheet" href="css/diocane.css">
 </head>
 <body> 
     <nav>
         <h1><?php echo $username ?> </h1>
     </nav>
     <?php 
-        include "php_scripts/connect_db.php";
         $conn = connect();
 
         $stmt = $conn->prepare("SELECT user_id, bio FROM User WHERE username = ?");
@@ -45,13 +51,33 @@
         
         echo "<div class='main-div'>";
         echo "<h1>Reviews</h1>";
+        
         $found = false;
         while ($review = $reviewsResult->fetch_assoc()) {
-            echo "<div class='review'>";
-            echo "<h2>" . htmlspecialchars($review['title']) . "</h2>";
-            //TODO album image (?)
-            echo "<p>" . nl2br(htmlspecialchars($review['corpus'])) . "</p>";
-            echo "</div>";
+            $album = search_album_by_id($token, $review['album_id']);
+            $image_url = $album['images'][0]['url']; 
+            $album_name = htmlspecialchars($album['name']);
+            $album_release_date = htmlspecialchars($album['release_date']);
+            $album_artist = htmlspecialchars($album['artists'][0]['name']);
+            ?>
+
+            <div class='review'>
+                <a class="review-left" href="album.php?album_id=<?php echo $album['id'] ?>">
+                    <div>
+                        <img src="<?php echo $image_url; ?>" alt="Album Image">
+                        <div class="album-info">
+                            <h3><?php echo $album_name; ?></h3>
+                            <p>Artist: <?php echo $album_artist; ?></p>
+                            <p>Released: <?php echo $album_release_date; ?></p>
+                        </div>
+                    </div>
+                </a>
+                <div class="review-right">
+                    <h2><?php echo htmlspecialchars($review['title']); ?></h2>
+                    <p><?php echo nl2br(htmlspecialchars($review['corpus'])); ?></p>
+                </div>
+            </div>
+            <?php
             $found = true;
         }
 
