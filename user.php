5439ba72e07694270393b8ce921af21da90f484a
Nikicoraz <nicolacorato05@gmail.com>
2024-05-24 19:12:17 +0200

    Css problematico

--- user.php
+++ user.php
@@ -17,6 +17,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $username ?></title>
+    <link rel="stylesheet" href="css/comune.css">
+    <link rel="stylesheet" href="css/user.css">
 </head>
 <body> 
     <nav>
