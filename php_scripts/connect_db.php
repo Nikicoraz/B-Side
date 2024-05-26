e11f35b9a85447d2f1a9e6f05129e73fca146b7f
Nikicoraz <nicolacorato05@gmail.com>
2024-05-26 11:03:15 +0200

    DB_NAME in .env

--- php_scripts/connect_db.php
+++ php_scripts/connect_db.php
@@ -7,7 +7,7 @@
     function connect(){
         // Connect to the database
         $env = parse_ini_file(dirname(__FILE__) . "/../.env");
-        $conn = new mysqli($env['DB_URL'], $env["DB_USER"], $env["DB_PASS"], "bside");
+        $conn = new mysqli($env['DB_URL'], $env["DB_USER"], $env["DB_PASS"], $env["DB_NAME"]);
         // If connection fails, stop the script
         if($conn->connect_error){
             die("Connection failed: " . $conn->connect_error);
